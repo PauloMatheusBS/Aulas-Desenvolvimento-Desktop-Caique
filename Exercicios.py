d0726f6cbec266cfcee00a439918433b5d67d14a
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2024-10-15 08:49:16 -0400

    att 08:49

--- Exercicios.py
+++ Exercicios.py
@@ -11,21 +11,24 @@
 #         for i in range(2, n + 1):
 #             resultado *= i
 #         return resultado
-
 # num = int(input("Digite um número: "))
 # print(f"O fatorial de {num} é {fatorial(num)}.")
 
 #############################################################################################################################################
 #2 - Desenvolva um programa que mostre a tabuada de um número informado pelo usuário.
-def tabuada (num):
-    for i in range (1,11):
-        result = num * i
-        print(f"{num} X {i} =",result)
-        i += 1
-num = int(input("Digite um número: "))
-tabuada(num)
+# def tabuada (num):
+#     for i in range (1,11):
+#         result = num * i
+#         print(f"{num} X {i} =",result)
+#         i += 1
+# num = int(input("Digite um número: "))
+# tabuada(num)
 
+#############################################################################################################################################
 #3 - Escreva uma função que verifique se uma palavra ou frase é um palíndromo (pode ser lida da mesma maneira de trás para frente).
+def palindromo ():
+
+
 #4 - Faça um programa que verifique se um número fornecido é primo.
 #5  - Desenvolva uma função que mostre os n primeiros termos da sequência de Fibonacci, onde n é informado pelo usuário.
 #6 - Escreva um algoritmo que receba uma lista de números e retorne a lista ordenada de forma crescente (Bubble sort).
