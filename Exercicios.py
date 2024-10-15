25af7102e0f03cd729f4a2558aa4acd42633339b
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2024-10-15 08:45:21 -0400

    att ex 2

--- Exercicios.py
+++ Exercicios.py
@@ -1,22 +1,30 @@
 # Lista de Exercícios (10/10)
 
 #1 - Crie uma função que calcule o fatorial de um número dado pelo usuário.
-def fatorial(n):
-    if n < 0:
-        return "Deu RUIM, n pode ser negativo"
-    elif n == 0 or n == 1:
-        return 1
-    else:
-        resultado = 1
-        for i in range(2, n + 1):
-            resultado *= i
-        return resultado
+# def fatorial(n):
+#     if n < 0:
+#         return "Deu RUIM, n pode ser negativo"
+#     elif n == 0 or n == 1:
+#         return 1
+#     else:
+#         resultado = 1
+#         for i in range(2, n + 1):
+#             resultado *= i
+#         return resultado
 
-num = int(input("Digite um número: "))
-print(f"O fatorial de {num} é {fatorial(num)}.")
+# num = int(input("Digite um número: "))
+# print(f"O fatorial de {num} é {fatorial(num)}.")
 
 #############################################################################################################################################
 #2 - Desenvolva um programa que mostre a tabuada de um número informado pelo usuário.
+def tabuada (num):
+    for i in range (1,11):
+        result = num * i
+        print(f"{num} X {i} =",result)
+        i += 1
+num = int(input("Digite um número: "))
+tabuada(num)
+
 #3 - Escreva uma função que verifique se uma palavra ou frase é um palíndromo (pode ser lida da mesma maneira de trás para frente).
 #4 - Faça um programa que verifique se um número fornecido é primo.
 #5  - Desenvolva uma função que mostre os n primeiros termos da sequência de Fibonacci, onde n é informado pelo usuário.
