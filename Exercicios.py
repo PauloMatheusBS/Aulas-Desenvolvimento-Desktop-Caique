27be0774adf25fe5f60d9a24dab76c2c1d4a68a5
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2024-10-15 09:18:10 -0400

    att ex4

--- Exercicios.py
+++ Exercicios.py
@@ -26,11 +26,34 @@
 
 #############################################################################################################################################
 #3 - Escreva uma função que verifique se uma palavra ou frase é um palíndromo (pode ser lida da mesma maneira de trás para frente).
-def palindromo ():
+# def palindromo(frase):
+#     return frase == frase[::-1]
 
+# entrada = input("Digite a palavra: ")
+# if palindromo(entrada):
+#     print("É um palíndromo!")
+# else:
+#     print("Não é um palíndromo.")
 
+#############################################################################################################################################
 #4 - Faça um programa que verifique se um número fornecido é primo.
+# def primo(entrada):
+#     if entrada <= 1:
+#         print(f"O número {entrada} não é primo!")
+#         return
+    
+#     for i in range(2, int(entrada**0.5) + 1):
+#         if entrada % i == 0:
+#             print(f"O número {entrada} não é primo!")
+#             return
+    
+#     print(f"O número {entrada} é primo!")
+
+# entrada = int(input("Digite o número: "))
+# primo(entrada)
+#############################################################################################################################################
 #5  - Desenvolva uma função que mostre os n primeiros termos da sequência de Fibonacci, onde n é informado pelo usuário.
+#for _ in range(n):     seq.append(a)     a, b = b, a + b
 #6 - Escreva um algoritmo que receba uma lista de números e retorne a lista ordenada de forma crescente (Bubble sort).
 #7 - Faça um programa que converta uma temperatura de Celsius para Fahrenheit e vice-versa. O usuário deverá escolher a conversão desejada.
-#8 - Crie um algoritmo que receba 5 números e exiba o maior e o menor número informados.
+#8 - Crie um algoritmo que receba 5 números e exiba o maior e o menor número informados
