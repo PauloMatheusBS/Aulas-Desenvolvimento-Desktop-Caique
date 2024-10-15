cd8b4eb8ed4348732d918fd9711ee1117f881834
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2024-10-15 08:29:17 -0400

    tete 08:28

--- Exercicios.py
+++ Exercicios.py
@@ -1,4 +1,10 @@
-print("Testeeeee")
-print("Testeeeee")
-print("Testeeeee")
-print("Testeeeee")
+# Lista de Exercícios (10/10)
+
+#1 - Crie uma função que calcule o fatorial de um número dado pelo usuário.
+#2 - Desenvolva um programa que mostre a tabuada de um número informado pelo usuário.
+#3 - Escreva uma função que verifique se uma palavra ou frase é um palíndromo (pode ser lida da mesma maneira de trás para frente).
+#4 - Faça um programa que verifique se um número fornecido é primo.
+#5  - Desenvolva uma função que mostre os n primeiros termos da sequência de Fibonacci, onde n é informado pelo usuário.
+#6 - Escreva um algoritmo que receba uma lista de números e retorne a lista ordenada de forma crescente (Bubble sort).
+#7 - Faça um programa que converta uma temperatura de Celsius para Fahrenheit e vice-versa. O usuário deverá escolher a conversão desejada.
+#8 - Crie um algoritmo que receba 5 números e exiba o maior e o menor número informados.
