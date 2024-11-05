d2fc0bfc70f5e1e6943ac7d9cb9ee4653d905d15
Paulo Matheus <paulo54098136@aluno.ms.senac.br>
2024-11-05 09:05:04 -0400

    09:05

--- Biblioteca/Livros.py
+++ Biblioteca/Livros.py
@@ -1,3 +1,5 @@
+import mysql.connector
+
 class Livro():
     def __init__(self,titulo,autor,genero,cod_livro):
         self.titulo = titulo
@@ -23,4 +25,9 @@ class Livro():
         self.usuario = None
         self.status = "Disponivel"
 
-
+    def criar_livro(self):
+        teste = 0        
+        teste.execute('INSERT INTO livro(titulo, autor, genero, status_livro, codigo) VALUES ("O pequeno principe", "Enzo", "Fantasia", "Disponivel", 5106106)') 
+        teste.commit()
+        print("Registro inserido com sucesso.")
+    
